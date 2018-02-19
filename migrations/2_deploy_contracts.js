e8877a3b11452b8e84e09d47afa6b86f1a12a899
yifon <yolina_379653734@qq.com>
2018-02-19 15:03:07 +0800

    测试3个节点互连

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,8 @@
 //声明一个新到合约文件实例并命名为Test
-var Test = artifacts.require("./Test.sol");
-var ConvertLib=artifacts.require("./ConvertLib.sol");
-var MetaCoin=artifacts.require("./MetaCoin.sol");
+var Btm = artifacts.require("./BTM.sol");
 
 //部署合约
 module.exports = deployer => {
-    deployer.deploy(Test);
-    deployer.deploy(ConvertLib);
-    deployer.link(ConvertLib,MetaCoin);
-    deployer.deploy(MetaCoin);
+    deployer.deploy(Btm);
 }
 //truffle migrate --reset来强制重编译并发布所有合约
